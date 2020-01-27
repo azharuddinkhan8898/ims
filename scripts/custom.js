2cd5d8564df87882303e7c3f8ab55b9669c6468c
Khan <azharruddin.khan@kinesso.com>
2020-01-27 15:08:19 +0530

    home banner animation

--- scripts/custom.js
+++ scripts/custom.js
@@ -182,4 +182,40 @@ $(function() {
     $(this).toggleClass("active");
     $("#main-nav").slideToggle();
   });
+  var lFollowX = 0,
+    lFollowY = 0,
+    x = 0,
+    y = 0,
+    friction = 1 / 30;
+
+  function moveBackground() {
+    x += (lFollowX - x) * friction;
+    y += (lFollowY - y) * friction;
+    
+    translate = 'translate(' + x + 'px, ' + y + 'px) scale(1.1)';
+    translate1 = 'translate(' + -x + 'px, ' + -y + 'px) scale(1.1)';
+    $('.home-banner-wrapper .bg').css({
+      '-webit-transform': translate,
+      '-moz-transform': translate,
+      'transform': translate
+    });
+    $('.home-banner-wrapper .content-wrapper').css({
+      '-webit-transform': translate1,
+      '-moz-transform': translate1,
+      'transform': translate1
+    });
+
+    window.requestAnimationFrame(moveBackground);
+  }
+
+  $(window).on('mousemove click', function(e) {
+
+    var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
+    var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
+    lFollowX = (20 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
+    lFollowY = (10 * lMouseY) / 100;
+
+  });
+
+  moveBackground();
 });
