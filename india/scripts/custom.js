416cb6f9a3947c9c85f9ed6007be2cb065ae5a28
Ayushi <ayushi@M16MBHDSXG8WP.local>
2020-02-12 20:28:21 +0530

    india

--- india/scripts/custom.js
+++ india/scripts/custom.js
@@ -81,9 +81,34 @@ $(function() {
     });
   }
 
-  $(".learn-more-about-pets-slider").slick({
+  $(".learn-more-about-pets-slider, .dog-uniqueness .row").slick({
     slidesToShow: 4,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    infinite:false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
   });
   if ($(".b-slider").length) {
     $(".b-slider").slick({
